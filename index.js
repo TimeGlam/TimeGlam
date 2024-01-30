84d8ede12ff04c622fbbd36b07e6e190239cf887
bruno <magalhaes.brunno1@gmail.com>
2024-01-29 21:33:53 -0300

    initial config

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.set('port', 3000);
+
+app.listen(app.get('port'), () => {
+  console.log(`escutando na porta  ${app.get('port')}`);
+});
